7af79f95caefcc36a428f09319da75b4945e19e8
llya <2021454448@student.uitm.edu.my>
2023-04-18 16:52:27 +0000

    first commit

---
+++ lab1/userinput.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+/* This is my second program in C */
+int age;
+printf("Hi, how old are you? > ");
+scanf("%d", &age);
+printf("You are %d years old\n", age);
+return (0);
+}
